4b0ad43ba2933ac04b52545ca3843644e4a46ab4
Vaibhav Sharma <vs196@cs.duke.edu>
2022-07-27 10:38:28 -0400

    Sorted dataloader files and saved metrics and sizes arrays. Moved code into a main method

--- experiment.py
+++ experiment.py
@@ -58,7 +58,9 @@ def from_manual_segmentation_dataloader_with_size(manual_seg_dir,batch_size,num_
         for file in files:
             if file.endswith(".npy"):
                 filepaths.append(os.path.join(root,file))
-    filepaths=  filepaths[:size]
+    #sort by patient id
+    filepaths = sorted(filepaths, key=lambda x:x.split("/")[-1])
+    filepaths = filepaths[0:size]
     new_unet_dataloader = unet_dataloader(filepaths,batch_size,num_workers)
     return new_unet_dataloader
 
@@ -107,43 +109,46 @@ def evaluate_metric_on_validation(model,validation_dir):
     
     
 
+def control_run():
+    #Initialize filepaths 
+    im_dir = "/usr/xtmp/vs196/mammoproj/Data/manualfa/train/" #manually labelled training segmentations
+    manual_fa_train_dir = "/usr/xtmp/vs196/mammoproj/Data/manualfa/train/"
+    model_save_dir = "/usr/xtmp/vs196/mammoproj/Code/SavedModels/ControlALUNet/0726/"
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir)
+    sizes = []
+    metrics = []
+    for size in range(10,200,10):
+        #initialize dataloader and model
+        manual_fa_dataloader = from_manual_segmentation_dataloader_with_size(manual_fa_train_dir,10,2,size)
+        unet_model = getattr(ternausnet.models, "UNet16")(num_classes=2,pretrained=True).cuda()
+
+        #train model
+        unet_model,loss_tracker,metric_tracker = unet_update_model(unet_model,manual_fa_dataloader,num_epochs=25)
+
+        #save model
+        model_save_path = model_save_dir + f"unetmodel_size{size}.pth"
+        torch.save(unet_model,model_save_path)
+
+        manual_fa_valid_dir = f"/usr/xtmp/vs196/mammoproj/Data/manualfa/manual_validation/"
+        metric = evaluate_metric_on_validation(unet_model,manual_fa_valid_dir)
+        sizes.append(size)
+        metrics.append(metric)
+        print(f"Done with size={size}. Metric={metric}")
+    print("Done with all sizes")
+    plt.scatter(sizes,metrics)
+    sizes_np = np.asarray(sizes)
+    metrics_np = np.asarray(metrics)
+    data_save_dir = "/usr/xtmp/vs196/mammoproj/Code/ActiveLearning/0727controlrun/"
+    if not os.path.exists(data_save_dir):
+        os.makedirs(data_save_dir)
+    sizes_save_path = "/usr/xtmp/vs196/mammoproj/Code/ActiveLearning/0727controlrun/sizes.npy"
+    metrics_save_path = "/usr/xtmp/vs196/mammoproj/Code/ActiveLearning/0727controlrun/metrics.npy"
+    np.save(sizes_save_path,sizes_np)
+    np.save(metrics_save_path,metrics_np)
+    plt.savefig("/usr/xtmp/vs196/mammoproj/Code/ActiveLearning/0727controlrun/control_graph.png")
+    print("Done")
 
 
-#Initialize filepaths 
-im_dir = "/usr/xtmp/vs196/mammoproj/Data/manualfa/train/" #manually labelled training segmentations
-manual_fa_train_dir = "/usr/xtmp/vs196/mammoproj/Data/manualfa/train/"
-model_save_dir = "/usr/xtmp/vs196/mammoproj/Code/SavedModels/ControlALUNet/0726/"
-if not os.path.exists(model_save_dir):
-    os.makedirs(model_save_dir)
-sizes = []
-metrics = []
-for size in range(10,200,10):
-    #initialize dataloader and model
-    manual_fa_dataloader = from_manual_segmentation_dataloader_with_size(manual_fa_train_dir,10,2,size)
-    unet_model = getattr(ternausnet.models, "UNet16")(num_classes=2,pretrained=True).cuda()
-
-    #train model
-    unet_model,loss_tracker,metric_tracker = unet_update_model(unet_model,manual_fa_dataloader,num_epochs=25)
-
-    #save model
-    model_save_path = model_save_dir + f"unetmodel_size{size}.pth"
-    torch.save(unet_model,model_save_path)
-
-    manual_fa_valid_dir = f"/usr/xtmp/vs196/mammoproj/Data/manualfa/manual_validation/"
-    metric = evaluate_metric_on_validation(unet_model,manual_fa_valid_dir)
-    sizes.append(size)
-    metrics.append(metric)
-    print(f"Done with size={size}. Metric={metric}")
-print("Done with all sizes")
-
-
-
-plt.scatter(sizes,metrics)
-plt.savefig("/usr/xtmp/vs196/mammoproj/Code/ActiveLearning/control_graph.png")
-
-
-print("Done")
-
-
-
-
+if __name__ == "__main__":
+    control_run()
